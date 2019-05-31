c66ba9b86ba2bed1df5b6a408f9fa024338592c6
Zach Kirsch <zkirsch@palantir.com>
2019-05-31 08:55:35 -0400

    publishing site

---
+++ marriott_matrix/precache-manifest.0769bf693c7c661a7e43e6431ecdf621.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee184fbefeab2664d4598f7209069cb8",
+    "url": "/marriott_matrix/index.html"
+  },
+  {
+    "revision": "52205b54965a66c56dca",
+    "url": "/marriott_matrix/static/css/2.656aabcf.chunk.css"
+  },
+  {
+    "revision": "8b8f3b431452af206cdd",
+    "url": "/marriott_matrix/static/css/main.eb708a86.chunk.css"
+  },
+  {
+    "revision": "52205b54965a66c56dca",
+    "url": "/marriott_matrix/static/js/2.2882bb3a.chunk.js"
+  },
+  {
+    "revision": "8b8f3b431452af206cdd",
+    "url": "/marriott_matrix/static/js/main.4101a3b5.chunk.js"
+  },
+  {
+    "revision": "6c2a5f638c3860a6808e",
+    "url": "/marriott_matrix/static/js/runtime~main.d1f56ff8.js"
+  }
+]);
--- marriott_matrix/service-worker.js
+++ marriott_matrix/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e7684eb75a15d0f26ff6e4dcd48b918.js"
+  "/marriott_matrix/precache-manifest.0769bf693c7c661a7e43e6431ecdf621.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/marriott_matrix/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
